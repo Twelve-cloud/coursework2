6985255445503522a4e93d64bd51afc0427af0d6
Twelve <twelve@cloud.com>
2021-04-15 19:06:41 +0300

    added getInstance() function for MYSQL* obj and createConnection() function for creating connection with database

--- server/mysql_api.h
+++ server/mysql_api.h
@@ -1,12 +1,31 @@
 #ifndef MYSQL_API_H
 #define MYSQL_API_H
 
+#include "mysql_api_exception.h"
 #include <mysql.h>
 #include <iostream>
 
 class MySqlAPI
 {
 private:
+    MYSQL* getInstance()
+    {
+        MYSQL* tmpConnection = mysql_init(nullptr);
+
+        if (tmpConnection == nullptr)
+        {
+            throw MySqlException::InitializationFailed(mysql_error(connection), mysql_errno(connection));
+        }
+
+        return tmpConnection;
+    }
+    void createConnection()
+    {
+        if (mysql_real_connect(connection, host.c_str(), user.c_str(), userPassword.c_str(), databaseName.c_str(), 0L, nullptr, 0L) == nullptr)
+        {
+            throw MySqlException::ConnectionFailed(mysql_error(connection), mysql_errno(connection));
+        }
+    }
 
 private:
     MYSQL* connection;
